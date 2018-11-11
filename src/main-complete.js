92deec25fffaf3717462efae5f86982565c95e38
michstallings <contact@michellestallings.com>
2018-11-11 12:58:28 -0800

    Added changColor steps

--- src/main-complete.js
+++ src/main-complete.js
@@ -2,10 +2,16 @@ const navButtons = document.querySelectorAll('#navbar ul li');
 const navBar = document.querySelector('#navbar');
 
 function wrapper() {
+
+	const changColor = () => {
+		navBar.classList.toggle('red');
+	};
+
   const loopThrough = () => {
 
 
     for (let i = 0; i < thisButtonLength; i++) {
+    	changColor();
 
     }
 
